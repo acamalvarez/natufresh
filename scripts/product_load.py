fc205251aa15ffa894cbe14941f77ce4299f94ee
Camilo Álvarez <acalvm@gmail.com>
2020-11-03 21:21:31 -0500

    update product_load script and adding .gitignore

--- scripts/product_load.py
+++ scripts/product_load.py
@@ -1,2 +1,42 @@
+import csv
+
 from accounts.models import Product, Tag
 
+def run():
+    fhand = open('static/products.csv')
+    reader = csv.reader(fhand)
+    next(reader)
+
+    Product.objects.all().delete()
+    Tag.objects.all().delete()
+
+    for row in reader:
+
+        try:
+            id=row[0]
+            name = row[1]
+            category = row[2]
+            tag = row[3]
+            price = float(row[4])
+            description = row[5]
+        except:
+            id = None
+            name = None
+            category = None
+            tag = None
+            price = None
+            description = None
+
+        tag, created = Tag.objects.get_or_create(name=row[3])
+
+        product, created = Product.objects.get_or_create(id=id,
+            name=name, price=price, category=category, 
+            description=description)
+
+        product.tags.add(tag)
+        
+        product.save()
+
+
+# For updating the product list run the following script
+# python manage.py runscript product_load
