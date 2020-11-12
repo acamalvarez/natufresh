e0c86ebcaebb7298403ab22bbb6395d42055359d
Camilo Álvarez <acalvm@gmail.com>
2020-11-12 11:41:30 -0500

    testing new configurations

--- natufresh/settings.py
+++ natufresh/settings.py
@@ -157,10 +157,11 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_URL = '/static/'
 
-# uncomment to work on local
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
+
+STATIC_DIRS = 'https://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/static'
 
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
