ba450a18cc0b4c4d1ef7581c755597dca0b67688
Camilo <acalvm@gmail.com>
2021-09-11 17:56:58 -0500

    Set comment to testing user in login

--- natufresh/settings.py
+++ natufresh/settings.py
@@ -107,7 +107,7 @@ WSGI_APPLICATION = 'natufresh.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'natufresh_db',
+        'NAME': env('DATABASE_NAME'),
         'USER': env('DATABASE_USER'),
         'PASSWORD': env('DATABASE_PASSWORD'),
         'HOST': env('DATABASE_HOST'),
